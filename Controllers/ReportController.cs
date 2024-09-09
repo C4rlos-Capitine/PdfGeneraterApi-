0acf38f629d1b53f11915c058f6d99a69849c6a3
Carlos Mutemba <carlos.mutemba@academy.compass.uol>
2024-09-09 21:40:08 +0200

    all files

---
+++ Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Rotativa.AspNetCore;
+
+namespace PdfGeneraterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet("factura")]
+        public async Task<IActionResult> Factura()
+        {
+            ViewAsPdf vista = new ViewAsPdf();
+            byte[] dados = await vista.BuildFile(ControllerContext);
+            return new FileContentResult(dados, "application/pdf") { 
+                FileDownloadName = "Factura.pdf"
+            };
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using Rotativa.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddControllersWithViews();//adicionado por mim
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+//configuracoes para rotativa
+String wwwroot = app.Environment.WebRootPath;
+
+RotativaConfiguration.Setup(wwwroot, "rotativa");
+
+
+app.Run();
